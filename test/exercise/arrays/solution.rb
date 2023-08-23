1305f68a8f1733b00cec1bce24c371bee1a5ba37
ZayatsAliaksei <lehazayac@tut.by>
2023-08-24 00:20:45 +0400

    arrays add methods

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,13 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |item| item > 0 ? array.max() : item }
       end
 
       def search(_array, _query)
-        0
+        return -1 unless _array.include?(_query)
+
+        _array.bsearch_index {|x| x >= _query}
       end
     end
   end
