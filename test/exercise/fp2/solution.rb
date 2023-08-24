c2bafd9a59602a2b54fa687af162fca81cace7e6
ZayatsAliaksei <lehazayac@tut.by>
2023-08-25 00:16:55 +0400

    my functions

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,20 +1,38 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      # Использовать стандартные функции массива для решения задач нельзя.
-      # Использовать свои написанные функции для реализации следующих - можно.
-
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for item in self
+          yield(item)
+        end
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        my_reduce(MyArray.new) do |result, element|
+          result << yield(element)
+        end
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(MyArray.new) do |result, element|
+          result << element unless element.nil?
+          result
+        end
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(initial = nil)
+        accumulator = initial || self[0]
+        index = initial ? 0 : 1
+        while index < length
+          accumulator = yield(accumulator, self[index])
+          index += 1
+        end
+        accumulator
+      end
     end
   end
 end
