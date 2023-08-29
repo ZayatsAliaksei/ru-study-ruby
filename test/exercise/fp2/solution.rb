c402b4d2bb2b14c993a0ddacad99ac5483e7fbf9
ZayatsAliaksei <lehazayac@tut.by>
2023-08-29 16:29:45 +0400

    fix by comments2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,7 +6,8 @@ module Exercise
         return self unless block_given?
 
         yield(first)
-        self[1..].my_each(&block)
+        MyArray.new(self[1..]).my_each(&block) unless self[1..].empty?
+        self
       end
 
       # Написать свою функцию my_map
@@ -26,10 +27,14 @@ module Exercise
 
       # Написать свою функцию my_reduce
       def my_reduce(acc = nil, &block)
-        return self unless block_given?
+        return acc unless block_given?
 
         acc = acc.nil? ? first : block.call(acc, first)
-        self[1..].my_reduce(acc, &block)
+        if size > 1
+          rest = MyArray.new(self[1..])
+          acc = rest.my_reduce(acc, &block)
+        end
+        acc
       end
     end
   end
